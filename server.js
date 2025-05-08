1c2abb52d0dcc387e5a03d6e4d226dbdb82d9c13
Lea354 <frischknecht-leahotmail.com>
2025-05-08 17:46:58 +0200

    anpassung server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Cloud Deploy auf Azure with GitHub Actions!');
+  res.send('Hello FS2025 DevOps Course! Es ist Donnerstagabend und ich (Lea) mache eine Anpassung am Code');
 });
 
 app.listen(PORT, HOST);
